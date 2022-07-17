a93c1054ba7282caab33bbd824f490408d5a56be
Ben Edivri <eddyjamin@gmail.com>
2022-07-17 02:09:47 +0100

    Update animated_list_view.dart

--- lib/screens/home/widgets/animated_list_view.dart
+++ lib/screens/home/widgets/animated_list_view.dart
@@ -10,24 +10,26 @@ class AnimatedListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       alignment: Alignment.bottomCenter,
+      
       children: [
         ListData(
+          
           subtitle: 'With Jane, Cape Town',
           title: 'Mastering Flutter',
           margin: listSlidePosition.value * 1,
-          image: const AssetImage('images/babe.jpg'),
+          image: const AssetImage('images/modell.jpg'),
         ),
         ListData(
           subtitle: 'Camps Bay',
           title: 'Working with Dart',
           margin: listSlidePosition.value * 0,
-          image: const AssetImage('images/babe.jpg'),
+          image: const AssetImage('images/modell.jpg'),
         ),
         ListData(
           subtitle: 'Dubai',
           title: 'Working with Animation',
           margin: listSlidePosition.value * 0,
-          image: const AssetImage('images/babe.jpg'),
+          image: const AssetImage('images/modell.jpg'),
         ),
       ],
     );
